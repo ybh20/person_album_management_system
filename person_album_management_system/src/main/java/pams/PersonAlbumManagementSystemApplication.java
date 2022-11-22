fc26ee310a9f96524df04cec7983b613108554e0
hg <3350321816@qq.com>
2022-11-23 01:27:31 +0800

    环境搭建

---
+++ person_album_management_system/src/main/java/pams/PersonAlbumManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package pams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonAlbumManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonAlbumManagementSystemApplication.class, args);
+	}
+
+}
---
+++ person_album_management_system/src/main/java/pams/bean/User.java
@@ -0,0 +1,10 @@
+package pams.bean;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+	private String name;
+
+}
